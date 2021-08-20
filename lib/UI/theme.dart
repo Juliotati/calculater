0d99bc074c637fea6f12e4e3177fd328a46aa448
juliotati <59662912+Juliotati@users.noreply.github.com>
2021-08-20 14:50:48 +0100

    removed dead code

--- lib/UI/theme.dart
+++ lib/UI/theme.dart
@@ -3,6 +3,5 @@ part of ui;
 final ThemeData calculaterTheme = ThemeData(
   primarySwatch: Colors.blueGrey,
   visualDensity: VisualDensity.adaptivePlatformDensity,
-  textTheme: GoogleFonts.poppinsTextTheme(),
-  // textTheme: GoogleFonts.pressStart2pTextTheme(),
+  textTheme: GoogleFonts.pressStart2pTextTheme(),
 );
