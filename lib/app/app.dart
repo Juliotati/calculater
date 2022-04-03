b6df0e0bc1d298eda33fc02c9da75b1337849aaf
_juliotati <camilo.julio77@gmail.com>
2022-04-03 16:11:37 +0100

    chore: add max and min window size

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,7 @@
 library app;
 
-import 'dart:async';
-import 'dart:developer';
-
 import 'package:calculater/UI/ui.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 part 'calculater_page.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+import 'dart:io';
+
 import 'package:calculater/UI/ui.dart';
 import 'package:calculater/app/app.dart';
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    const Calculater(),
-  );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await DesktopWindow.setMinWindowSize(const Size(372.0, 700.0));
+    await DesktopWindow.setMaxWindowSize(const Size(372.0, 700.0));
+  }
+  runApp(const Calculater());
 }
 
 class Calculater extends StatelessWidget {
   const Calculater();
+
   @override
   Widget build(BuildContext context) {
     return ListenableProvider<CalculaterHelper>(
